68fc4805747c901e7714d373c2d1a3fd3322567e
istiakiraz <istiakiraz@gmail.com>
2025-10-16 10:31:25 +0600

    recyclingProcess page responsive

--- src/components/RecyclingProcessComponents/RecyclingHeader.jsx
+++ src/components/RecyclingProcessComponents/RecyclingHeader.jsx
@@ -5,7 +5,7 @@ import PrimaryButton from '../../common/PrimaryButton'
 
 export default function RecyclingHeader() {
   return (
-    <section className="relative   mx-auto  py-30   flex items-center justify-center text-white overflow-hidden">
+    <section className="relative   mx-auto py-10 lg:py-30   flex items-center justify-center text-white overflow-hidden">
           {/* Background Image + Gradient */}
           <div className="absolute inset-0 z-0">
             <img
@@ -17,14 +17,14 @@ export default function RecyclingHeader() {
           </div>
     
           {/* Content */}
-          <div className="relative z-10 w-9/12  text-center md:text-left">
-            <h1 className="text-3xl md:text-5xl w-8/12 font-bold mb-4 leading-tight">
+          <div className="relative z-10 2xl:max-w-9/12 max-w-11/12  text-left md:text-left">
+            <h1 className="text-3xl md:text-5xl lg:w-8/12 font-bold mb-4 leading-tight">
              The process of recycling photovoltaic panels
             </h1>
-            <p className="text-lg md:text-xl mr-auto w-6/12 mb-6 text-gray-200">
+            <p className="text-lg md:text-xl mr-auto lg:w-6/12 mb-6 text-gray-200">
             Exploring Sustainable Methods to Maximize Material Recovery and Minimize Waste in Photovoltaic Panel Recycling.
             </p>
-            <div className="flex flex-col md:flex-row items-center gap-4 justify-center md:justify-start">
+            <div className="flex flex-col md:flex-row items-start gap-4 justify-start">
              <PrimaryButton to="/marketplace" icon={FaArrowRightLong}>
     Browse Marketplace
              </PrimaryButton>
